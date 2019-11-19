0768c7d92dad5bb872741ab06c9af941e2df4058
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-19 10:52:02 +0700

    add secondary Color constructor from rgba color number

--- graphics/src/commonMain/kotlin/dev/icerock/moko/graphics/Color.kt
+++ graphics/src/commonMain/kotlin/dev/icerock/moko/graphics/Color.kt
@@ -19,4 +19,11 @@ data class Color(
             green.toLong().shl(8) or
             red.toLong().shl(16) or
             alpha.toLong().shl(24)
+
+    constructor(colorRGBA: Long) : this(
+        red = (colorRGBA.shr(24) and 0xFF).toInt(),
+        green = (colorRGBA.shr(16) and 0xFF).toInt(),
+        blue = (colorRGBA.shr(8) and 0xFF).toInt(),
+        alpha = (colorRGBA.shr(0) and 0xFF).toInt()
+    )
 }
