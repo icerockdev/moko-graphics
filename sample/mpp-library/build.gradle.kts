2a56ff98fc6f797d92028f16fa9d671e134b60ef
Anton Shestak <ashestak@icerockdev.com>
2021-06-08 11:01:36 +0700

    #21 Export correct artifact to use all classes in all projects

--- sample/mpp-library/build.gradle.kts
+++ sample/mpp-library/build.gradle.kts
@@ -11,6 +11,11 @@ plugins {
 
 kotlin {
     macosX64()
+    targets.withType(org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget::class.java).all {
+        binaries.withType(org.jetbrains.kotlin.gradle.plugin.mpp.Framework::class.java).all {
+            export(projects.graphics)
+        }
+    }
 }
 
 dependencies {
